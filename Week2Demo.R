8d8ffc981a7ad3a4e96549b628de713f38f0b88a
Brian Gallagher <L00005877@atu.ie>
2026-09-23 11:29:39 +0100

    Test 3

--- Week2Demo.R
+++ Week2Demo.R
@@ -1,5 +1,41 @@
-x <- 2
+x <-c(10.4,5.6,3.1,6.4,21.7)
 x
-y <- 5
+y <-c(x,0,x)
 y
-z <- 7
+z <-2*x+y+1
+z
+
+min(x)
+max(x)
+range(x)
+length(x) 
+sum(x) 
+prod(x) 
+
+1:30
+
+30:1
+
+
+s3 <- seq(-100,100,by=.6)
+s3
+
+
+#define vector y
+y <- c(1, 4, 6, 9)
+y
+#define sequence x, make sure length matches the length of y
+x <- seq(from=0, to=20, along.with=y)
+
+#view sequence
+x
+
+
+x <-c(10.4, 5.6, 3.1, 6.4, 21.7)
+s5 <-rep(x, times=5)
+
+s5
+
+s6 <-rep(x, each=5)
+
+s6
